5ab7c79634a66031ef9511123b306cc1027ecc4d
kittyegg <barscatred@gmail.com>
2026-06-01 16:06:14 +0300

    Добавил звук для изменения денег в кошельке

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] private Wallet _wallet;
     [SerializeField] private AudioSource[] _grassBlockDestroySound;
+    [SerializeField] private AudioSource[] _moneyChangeSound;
+
     private int _grassSoundIndex = 0;
+    private int _moneyChangeSoundIndex = 0;
+
+    private int _money;
+
+    private void OnEnable()
+    {
+        _money = _wallet.Money;
+
+        _wallet.OnMoneyChanged += OnMoneyChanged;
+    }
+
+    private void OnDisable()
+    {
+        _wallet.OnMoneyChanged -= OnMoneyChanged;
+    }
 
     public void PlayBlockDestroySound(BlockScript block)
     {
@@ -18,5 +36,17 @@ public class AudioManager : MonoBehaviour
                 _grassSoundIndex = (_grassSoundIndex + 1) % _grassBlockDestroySound.Length;
                 break;
         }
+    }
+
+    private void OnMoneyChanged(int money)
+    {
+        var oldMoney = _money;
+        _money = money;
+
+        if (_moneyChangeSound.Length == 0 || oldMoney <= money)
+            return;
+
+        _moneyChangeSound[_moneyChangeSoundIndex].Play();
+        _moneyChangeSoundIndex = (_moneyChangeSoundIndex + 1) % _moneyChangeSound.Length;
     }
 }
