6131e20242de0c9d4e0e3bb629cd7a0123e8cf16
kittyegg <barscatred@gmail.com>
2026-06-07 02:23:46 +0300

    Теперь за разрушение блоков выдаются деньги

---
+++ Assets/Scripts/BlockDestoyReward.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BlockDestoyReward : MonoBehaviour
+{
+    [SerializeField] private SandPathBuildService _sandPathBuildService;
+    [SerializeField] private Wallet _wallet;
+    [SerializeField] private int _reward = 10;
+
+    private void OnEnable()
+    {
+        _sandPathBuildService.OnBlockDestroy += OnBlockDestroy;
+    }
+
+    private void OnDisable()
+    {
+        _sandPathBuildService.OnBlockDestroy -= OnBlockDestroy;
+    }
+
+    private void OnBlockDestroy(BlockScript _)
+    {
+        _wallet.AddMoney(_reward);
+    }
+}
