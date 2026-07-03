3bd6c1609740097b6761fe56ab612cd1883aeaed
kittyegg <barscatred@gmail.com>
2026-07-04 01:52:20 +0300

    Теперь можно регулировать параметры врагов на разных уровнях

--- Assets/Scripts/ScriptableObjects/Level.cs
+++ Assets/Scripts/ScriptableObjects/Level.cs
@@ -5,8 +5,14 @@ using System.Collections.Generic;
 public class Level : ScriptableObject
 {
     [SerializeField] private Enemy[] _enemiesPrefabs;
-    [SerializeField] private float _levelTimeSec = 10f; 
+    [SerializeField] private float _levelTimeSec = 10f;
+    [SerializeField] private float _enemiesHealthMultiplier = 1;
+    [SerializeField] private float _enemiesSpeedMultiplier = 1;
+    [SerializeField] private float _enemiesDamagingMultiplier = 1;
     
     public IReadOnlyCollection<Enemy> EnemiesPrefabs => _enemiesPrefabs;
     public float LevelTimeSec => _levelTimeSec;
+    public float EnemiesHealthMultiplier => _enemiesHealthMultiplier;
+    public float EnemiesSpeedMultiplier => _enemiesSpeedMultiplier;
+    public float EnemiesDamagingMultiplier => _enemiesDamagingMultiplier;
 }
