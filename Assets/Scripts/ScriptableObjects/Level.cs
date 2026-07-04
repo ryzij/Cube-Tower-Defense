e987ad5cf6e7d8cbd16a5e0d773eea72386f7060
kittyegg <barscatred@gmail.com>
2026-07-04 08:40:39 +0300

    Добавил уровни

--- Assets/Scripts/ScriptableObjects/Level.cs
+++ Assets/Scripts/ScriptableObjects/Level.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 [CreateAssetMenu(fileName = "Level_No", menuName = "Scriptable Objects/Level")]
 public class Level : ScriptableObject
 {
+    [SerializeField] private bool _shuffle = false;
     [Tooltip("Если элемент не задан, будет выбран случайный из списка префабов спавнера")]
     [SerializeField] private Enemy[] _enemiesPrefabs;
     [SerializeField] private float _levelTimeSec = 10f;
@@ -11,7 +13,8 @@ public class Level : ScriptableObject
     [SerializeField] private float _enemiesSpeedMultiplier = 1;
     [SerializeField] private float _enemiesDamagingMultiplier = 1;
     
-    public IReadOnlyCollection<Enemy> EnemiesPrefabs => _enemiesPrefabs;
+    public IReadOnlyCollection<Enemy> EnemiesPrefabs =>
+        _shuffle ? _enemiesPrefabs.OrderBy(x => Random.Range(int.MinValue, int.MaxValue)).ToList() : _enemiesPrefabs;
     public float LevelTimeSec => _levelTimeSec;
     public float EnemiesHealthMultiplier => _enemiesHealthMultiplier;
     public float EnemiesSpeedMultiplier => _enemiesSpeedMultiplier;
