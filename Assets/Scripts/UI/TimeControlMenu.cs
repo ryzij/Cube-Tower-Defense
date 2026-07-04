c3c6bf6ee941b3238ccbe02a069f7fa9d2949b47
kittyegg <barscatred@gmail.com>
2026-07-04 11:39:38 +0300

    Добавил кнопку 3x

--- Assets/Scripts/UI/TimeControlMenu.cs
+++ Assets/Scripts/UI/TimeControlMenu.cs
@@ -6,12 +6,14 @@ public class TimeControlMenu : MonoBehaviour
     [SerializeField] private Button _pauseBtn;
     [SerializeField] private Button _resumeBtn;
     [SerializeField] private Button _2xBtn;
+    [SerializeField] private Button _3xBtn;
 
     private void Start()
     {
         _pauseBtn.onClick.AddListener(SwitchPause);
         _resumeBtn.onClick.AddListener(() => Time.timeScale = 1f);
         _2xBtn.onClick.AddListener(() => Time.timeScale = 2f);
+        _3xBtn.onClick.AddListener(() => Time.timeScale = 3f);
     }
 
     public static void SwitchPause()
